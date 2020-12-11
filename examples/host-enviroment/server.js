4148b1057590fa67c7663bce29053ed8d4ea2e72
liupengfei <1608272694@qq.com>
2020-12-11 20:53:44 +0800

    chore(examples): copy the examples of obvious-core

---
+++ examples/host-enviroment/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const child_process = require('child_process');
+const path = require('path');
+const fs = require('fs');
+
+const app = new express();
+
+if (!fs.existsSync('./dist')) {
+    fs.mkdirSync('./dist');
+}
+
+const childProcess = child_process.exec('npm run watch');
+childProcess.stdout.pipe(process.stdout);
+childProcess.stderr.pipe(process.stderr);
+
+app.use(express.static(path.join(__dirname, './dist/')));
+
+app.listen('9999', () => {
+    setTimeout(() => {
+        child_process.execSync('start http://localhost:9999/index.html');
+    }, 3000);
+});
