e7b911612142320cad1ad0dc5c47aa12ccfda623
liupengfei <1608272694@qq.com>
2021-01-08 23:48:21 +0800

    chore(examples): fix bug of vue example

--- examples/vue-app/src/main.js
+++ examples/vue-app/src/main.js
@@ -4,16 +4,16 @@ import App from './App.vue';
 import ObviousVue from '../../../src/index'
 
 Vue.config.productionTip = false;
-const bus = window.__Bus__.host;
-const socket = bus.createSocket();
+const $bus = window.__Bus__.host;
+const $socket = $bus.createSocket();
 
 Vue.use(ObviousVue)
 
-bus.createApp('vue-app')
+$bus.createApp('vue-app')
   .bootstrap(async (config) => {
     new Vue({
       render: h => h(App),
-      bus,
-      socket
+      $bus,
+      $socket
     }).$mount(config.mountPoint);
   });
